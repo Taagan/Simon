6194e1baa09d30dd87d2d8e666784280e78514bb
Simon W <simon@wennolf.se>
2020-08-08 15:41:04 +0200

    whatever

--- Assignment3_Form/Form1.cs
+++ Assignment3_Form/Form1.cs
@@ -12,17 +12,26 @@ namespace Assignment3_Form
 {
     public partial class Form1 : Form
     {
-        Thread threadScan, threadArla, threadaxFood,threadICA,threadCOOP,threadCityGross,threadStorage;
+        Thread threadScan, threadArla, threadAxFood,threadICA,threadCOOP,threadCityGross,threadStorage;
         Semaphore semaphoreObject = new Semaphore(1,1);
         List<Item> storage = new List<Item>();
         List<Item> icaStorage = new List<Item>();
+        List<Item> cityGrossStorage = new List<Item>();
+        List<Item> coopStorage = new List<Item>();
+
         Random ran = new Random();
+        Item item = new Item("test", 1, 1);
         bool scan, arla, axFood,ica, coop, cityGross,storageCheck = true;
-        double icaCurrentItems, icaCurrentWeights, icaCurrentVolumes;
+        double icaCurrentItems, icaCurrentWeights, icaCurrentVolumes, coopCurrentItems, coopCurrentWeights, coopCurrentVolumes, cityGrossCurrentItems, cityGrossCurrentWeights, cityGrossCurrentVolumes;
         static System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
         public Form1()
         {
+           
             InitializeComponent();
+            
+            item.AddArlaProducts();
+            item.AddScanProducts();
+            item.AddAxFoodProducts();
         }
 
         public void GetRandomItem()
@@ -41,7 +50,7 @@ namespace Assignment3_Form
                 Invoke(inv);
             }
         }
-        public void Produce()
+        public void ScanProduce()
         {
             while (scan)
             {
@@ -52,7 +61,9 @@ namespace Assignment3_Form
                 }
                 else
                 {
-                    storage.Add(new Item("bread", 1, 1));
+                    int temp = ran.Next(0, item.scanItems.Count);
+                    temp = ran.Next(0, item.scanItems.Count);
+                    storage.Add(item.scanItems[temp]);
                 }
             }
             MethodInvoker inv = delegate
@@ -61,12 +72,63 @@ namespace Assignment3_Form
             };
             Invoke(inv);
         }
-        public void TakeFromStorage()
+
+        public void ArlaProduce()
         {
-            semaphoreObject.WaitOne();
+            while (arla)
+            {
+                Thread.Sleep(100);
+                if (storage.Count >= 50)
+                {
+
+                }
+                else
+                {
+                    int temp = ran.Next(0, item.arlaItems.Count);
+                    temp = ran.Next(0, item.arlaItems.Count);
+                    storage.Add(item.arlaItems[temp]);
+                }
+            }
+            MethodInvoker inv = delegate
+            {
+                lblStatusArla.Text = "Waiting";
+            };
+            Invoke(inv);
+        }
+
+
+        public void AxFoodProduce()
+        {
+            while (axFood)
+            {
+                Thread.Sleep(100);
+                if (storage.Count >= 50)
+                {
+
+                }
+                else
+                {
+                    int temp = ran.Next(0, item.axFoodItems.Count);
+                    temp = ran.Next(0, item.axFoodItems.Count);
+                    storage.Add(item.axFoodItems[temp]);
+                }
+            }
+            MethodInvoker inv = delegate
+            {
+                lblStatusAxfood.Text = "Waiting";
+            };
+            Invoke(inv);
+        }
+
+
+
+        public void IcaTakeFromStorage()
+        {
+            
             while (ica)
             {
-                Thread.Sleep(50);
+                semaphoreObject.WaitOne();
+                Thread.Sleep(200);
                 icaCurrentWeights = 0;
                 icaCurrentVolumes = 0;
                 foreach (Item item in icaStorage)
@@ -88,7 +150,7 @@ namespace Assignment3_Form
 
                     if (chkIcaCont.Checked)
                     {
-                        Thread.Sleep(5000);
+                        
                     }
                     else
                     {
@@ -101,8 +163,10 @@ namespace Assignment3_Form
                         ica = false;
                         //threadICA.Interrupt();
                     }
-                    semaphoreObject.Release();
+                    
+
                 }
+            
                 else
                 {
                     if (storage.Count > 0)
@@ -130,8 +194,170 @@ namespace Assignment3_Form
                         Thread.Sleep(100);
                     }
                 }
+                semaphoreObject.Release();
+                
             }
+            Thread.Sleep(5000);
         }
+
+        public void CoopTakeFromStorage()
+        {
+
+            while (coop)
+            {
+                semaphoreObject.WaitOne();
+                Thread.Sleep(200);
+                coopCurrentWeights = 0;
+                coopCurrentVolumes = 0;
+                foreach (Item item in coopStorage)
+                {
+                    coopCurrentItems = coopStorage.Count;
+                    coopCurrentWeights += item.weight;
+                    coopCurrentVolumes += item.volume;
+                }
+
+                if (storage.Count > 0 && (double.Parse(lblCoopItem.Text) <= coopCurrentItems || double.Parse(lblCoopWeight.Text) <= coopCurrentWeights + storage[storage.Count - 1].weight || double.Parse(lblCoopVolume.Text) <= coopCurrentVolumes + storage[storage.Count - 1].volume))
+                {
+                    MethodInvoker inv = delegate
+                    {
+                        lblCoopStatus.Text = "Limit Reached";
+                        coopStorage.Clear();
+                        lstCoop.Items.Clear();
+                    };
+                    Invoke(inv);
+
+                    if (chkCoopCont.Checked)
+                    {
+                        
+                    }
+                    else
+                    {
+                        MethodInvoker inv2 = delegate
+                        {
+                            lblCoopStatus.Text = "Waiting";
+
+                        };
+                        Invoke(inv);
+                        coop = false;
+                    }
+
+
+                }
+
+                else
+                {
+                    if (storage.Count > 0)
+                    {
+                        coopStorage.Add(storage[storage.Count - 1]);
+                        storage.Remove(storage[storage.Count - 1]);
+                        foreach (Item item in coopStorage)
+                        {
+                            MethodInvoker inv = delegate
+                            {
+                                lstCoop.Items.Add(item.name);
+                                lblCoopStatus.Text = "Loading";
+                            };
+                            Invoke(inv);
+                        }
+                    }
+                    else
+                    {
+                        MethodInvoker inv = delegate
+                        {
+                            lblCoopStatus.Text = "Waiting";
+
+                        };
+                        Invoke(inv);
+                        Thread.Sleep(100);
+                    }
+                }
+                semaphoreObject.Release();
+               
+            }
+            Thread.Sleep(5000);
+        }
+
+
+        public void CityGrossTakeFromStorage()
+        {
+
+            while (cityGross)
+            {
+                semaphoreObject.WaitOne();
+                Thread.Sleep(200);
+                cityGrossCurrentWeights = 0;
+                cityGrossCurrentVolumes = 0;
+                foreach (Item item in cityGrossStorage)
+                {
+                    cityGrossCurrentItems = cityGrossStorage.Count;
+                    cityGrossCurrentWeights += item.weight;
+                    cityGrossCurrentVolumes += item.volume;
+                }
+
+                if (storage.Count > 0 && (double.Parse(lblCityItem.Text) <= cityGrossCurrentItems || double.Parse(lblCityWeight.Text) <= cityGrossCurrentWeights + storage[storage.Count - 1].weight || double.Parse(lblCityVolume.Text) <= cityGrossCurrentVolumes + storage[storage.Count - 1].volume))
+                {
+                    MethodInvoker inv = delegate
+                    {
+                        lblCityStatus.Text = "Limit Reached";
+                        cityGrossStorage.Clear();
+                        lstCity.Items.Clear();
+                    };
+                    Invoke(inv);
+
+                    if (chkCityCont.Checked)
+                    {
+                        
+                    }
+                    else
+                    {
+                        MethodInvoker inv3 = delegate
+                        {
+                            lblCityStatus.Text = "Waiting";
+
+                        };
+                        Invoke(inv);
+                        cityGross = false;
+                        //threadICA.Interrupt();
+                    }
+
+
+                }
+
+                else
+                {
+                    if (storage.Count > 0)
+                    {
+                        cityGrossStorage.Add(storage[storage.Count - 1]);
+                        storage.Remove(storage[storage.Count - 1]);
+                        foreach (Item item in cityGrossStorage)
+                        {
+                            MethodInvoker inv = delegate
+                            {
+                                lstCity.Items.Add(item.name);
+                                lblCityStatus.Text = "Loading";
+                            };
+                            Invoke(inv);
+                        }
+                    }
+                    else
+                    {
+                        MethodInvoker inv = delegate
+                        {
+                            lblCityStatus.Text = "Waiting";
+
+                        };
+                        Invoke(inv);
+                        Thread.Sleep(100);
+                    }
+                }
+                semaphoreObject.Release();
+                
+            }
+            Thread.Sleep(5000);
+        }
+
+
+
 
         /// <summary>
         /// Start scan producer
@@ -140,7 +366,7 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStartScan_Click(object sender, EventArgs e)
         {
-            threadScan = new Thread(Produce);
+            threadScan = new Thread(ScanProduce);
             threadScan.Start();
             scan = true;
             btnStopScan.Enabled = true;
@@ -155,7 +381,12 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStartArla_Click(object sender, EventArgs e)
         {
-
+            threadArla = new Thread(ArlaProduce);
+            threadArla.Start();
+            arla = true;
+            btnStopArla.Enabled = true;
+            btnStartArla.Enabled = false;
+            lblStatusArla.Text = "Producing";
         }
 
         /// <summary>
@@ -165,7 +396,12 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStartAxfood_Click(object sender, EventArgs e)
         {
-
+            threadAxFood = new Thread(AxFoodProduce);
+            threadAxFood.Start();
+            axFood = true;
+            btnStopAxfood.Enabled = true;
+            btnStartAxfood.Enabled = false;
+            lblStatusAxfood.Text = "Producing";
         }
 
         /// <summary>
@@ -185,9 +421,17 @@ namespace Assignment3_Form
         {
             threadStorage = new Thread(StorageCheck);
             threadStorage.Start();
-            lblIcaItem.Text = "15";
-            lblIcaWeight.Text = "27,00";
-            lblIcaVolume.Text = "12,50";
+            lblIcaItem.Text = "13";
+            lblIcaWeight.Text = "12,00";
+            lblIcaVolume.Text = "30,50";
+
+            lblCoopItem.Text = "10";
+            lblCoopWeight.Text = "27,00";
+            lblCoopVolume.Text = "13,50";
+
+            lblCityItem.Text = "30";
+            lblCityWeight.Text = "50,10";
+            lblCityVolume.Text = "43,51";
         }
 
         /// <summary>
@@ -197,7 +441,9 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStopArla_Click(object sender, EventArgs e)
         {
-
+            arla = false;
+            btnStopArla.Enabled = false;
+            btnStartArla.Enabled = true;
         }
 
         /// <summary>
@@ -207,7 +453,9 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStopAxfood_Click(object sender, EventArgs e)
         {
-
+            axFood = false;
+            btnStopAxfood.Enabled = false;
+            btnStartAxfood.Enabled = true;
         }
 
         /// <summary>
@@ -217,7 +465,7 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStartIca_Click(object sender, EventArgs e)
         {
-            threadICA = new Thread(TakeFromStorage);
+            threadICA = new Thread(IcaTakeFromStorage);
             threadICA.Start();
             ica = true;
 
@@ -240,7 +488,9 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStartCoop_Click(object sender, EventArgs e)
         {
-
+            threadCOOP = new Thread(CoopTakeFromStorage);
+            threadCOOP.Start();
+            coop = true;
         }
 
         /// <summary>
@@ -250,7 +500,7 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStopCoop_Click(object sender, EventArgs e)
         {
-
+            coop = false;
         }
 
         /// <summary>
@@ -260,7 +510,9 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStartCity_Click(object sender, EventArgs e)
         {
-
+            threadCityGross = new Thread(CityGrossTakeFromStorage);
+            threadCityGross.Start();
+            cityGross = true;
         }
 
         /// <summary>
@@ -270,7 +522,7 @@ namespace Assignment3_Form
         /// <param name="e"></param>
         private void btnStopCity_Click(object sender, EventArgs e)
         {
-
+            cityGross = false;
         }
 
         /// <summary>
--- Assignment3_Form/Item.cs
+++ Assignment3_Form/Item.cs
@@ -15,6 +15,10 @@ namespace Assignment3_Form
         public string name;
         public double weight, volume;
         float productionTime;
+        public List<Item> arlaItems = new List<Item>();
+        public List<Item> scanItems = new List<Item>();
+        public List<Item> axFoodItems = new List<Item>();
+
         public Item(string name, double weight, double volume)
         {
             this.name = name;
@@ -25,9 +29,65 @@ namespace Assignment3_Form
 
 
 
-        public void produce()
+        public void AddArlaProducts()
         {
-            StreamReader sr = new StreamReader(".Items.txt");
+            arlaItems.Clear();
+            StreamReader sr = new StreamReader("ArlaItems.txt");
+
+            while (!sr.EndOfStream)
+            {
+
+                string tempName = sr.ReadLine();
+                double tempWeight = double.Parse(sr.ReadLine());
+                double tempVolym = double.Parse(sr.ReadLine());
+
+                arlaItems.Add(new Item(tempName, tempWeight, tempVolym));
+            }
+            sr.Close();
+
         }
+
+        public void AddScanProducts()
+        {
+            scanItems.Clear();
+            StreamReader sr = new StreamReader("ScanItems.txt");
+
+            while (!sr.EndOfStream)
+            {
+
+                string tempName = sr.ReadLine();
+                double tempWeight = double.Parse(sr.ReadLine());
+                double tempVolym = double.Parse(sr.ReadLine());
+
+                scanItems.Add(new Item(tempName, tempWeight, tempVolym));
+            }
+            sr.Close();
+
+        }
+
+
+
+
+        public void AddAxFoodProducts()
+        {
+            axFoodItems.Clear();
+            StreamReader sr = new StreamReader("AxFoodItems.txt");
+
+            while (!sr.EndOfStream)
+            {
+
+                string tempName = sr.ReadLine();
+                double tempWeight = double.Parse(sr.ReadLine());
+                double tempVolym = double.Parse(sr.ReadLine());
+
+                axFoodItems.Add(new Item(tempName, tempWeight, tempVolym));
+            }
+            sr.Close();
+
+        }
+
+
+
+
     }
 }
